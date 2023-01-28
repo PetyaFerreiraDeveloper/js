8d7ee45c1b9ea4ecf395c9c1ea9b0d8bebe6b697
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-01-28 08:52:53 +0100

    tests

--- testing/tdd-js/testHttp/app.js
+++ testing/tdd-js/testHttp/app.js
@@ -1,9 +1,15 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 app.post('/users', (req, res) => {
-    
+    const { password, username } = req.body;
+    if (!password || !username) {
+        res.sendStatus(400);
+        return 
+    }
+    res.send({userId: 0});
 });
 
-export default app;
+module.exports = app;
--- testing/tdd-js/testHttp/app.test.js
+++ testing/tdd-js/testHttp/app.test.js
@@ -1,23 +1,51 @@
-import { describe } from 'node:test';
-import supertest from 'supertest';
-import app from './app';
+const request = require('supertest');
+const app = require('./app');
 
 describe('POST /users', () => {
 
     describe('given a username and password', () => {
         // should save the username and password to the database
         // should respond with a json object containing the user id
+        
         test('should respond with status code 200', async () => {
             const response = await request(app).post('/users').send({
                 username: 'username',
                 password: 'password'
             });
             expect(response.statusCode).toBe(200);
+        });
+
+        test('should specify json in the content type header', async () => {
+            const response = await request(app).post('/users').send({
+                username: 'username',
+                password: 'password'
+            });
+            expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+        });
+
+        test('response has UserId', async () => {
+            const response = await request(app).post('/users').send({
+                username: 'username',
+                password: 'password'
+            });
+            expect(response.body.userId).toBeDefined();
         })
-        // should specify json in the content type header
+
+
     });
 
     describe('when the username and password are missing', () => {
-        // should respond with status code 400
+        test('should respond with status code 400', async () => {
+            const bodyData = [
+                {username: 'username'},
+                {password: 'password'},
+                {}
+            ]
+            for (const body of bodyData) {
+                const response = await request(app).post('/users').send(body);
+                expect(response.statusCode).toBe(400);
+            }
+            
+        })
     })
 });
